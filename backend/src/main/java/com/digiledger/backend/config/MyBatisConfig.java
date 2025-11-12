9a9a559359045ddfad93c3a18a4e4bbc8399bf96
令狐鸣 <sonicming@gmail.com>
2025-11-12 09:35:31 +0800

    Add explicit MyBatis session configuration

---
+++ backend/src/main/java/com/digiledger/backend/config/MyBatisConfig.java
@@ -0,0 +1,33 @@
+package com.digiledger.backend.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.digiledger.backend.mapper")
+public class MyBatisConfig {
+
+    private static final String MAPPER_LOCATION_PATTERN = "classpath:/mapper/*.xml";
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources(MAPPER_LOCATION_PATTERN)
+        );
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
