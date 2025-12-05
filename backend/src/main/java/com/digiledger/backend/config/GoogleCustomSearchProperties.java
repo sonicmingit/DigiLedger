f4a672765587809a502428e9f86838db1af87623
haoym22 <haoym22@chinaunicom.cn>
2025-12-05 22:55:33 +0800

    .

--- backend/src/main/java/com/digiledger/backend/config/GoogleCustomSearchProperties.java
+++ backend/src/main/java/com/digiledger/backend/config/GoogleCustomSearchProperties.java
@@ -11,10 +11,10 @@ import org.springframework.stereotype.Component;
 public class GoogleCustomSearchProperties {
 
     /** API Key */
-    private String apiKey = "AIzaSyCE1jX6DJ_6o5w1zRSBoc6u7IpHqmB01sg";
+    private String apiKey = "";
 
     /** 自定义搜索引擎 ID */
-    private String cseId ="851b134570d8146be";
+    private String cseId ="";
 
     /** 请求地址 */
     private String endpoint = "https://www.googleapis.com/customsearch/v1";
